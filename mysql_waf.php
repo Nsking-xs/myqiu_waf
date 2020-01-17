2208a3b641ff0013b8135e5dab6f03b3fe2640f6
NSKING.CN <3014311561@qq.com>
2020-01-18 01:12:40 +0800

    Update mysql_waf.php
    修复了日志
    日志中增加了提交内容
    日志中增加了时间

--- mysql_waf.php
+++ mysql_waf.php
@@ -2,23 +2,37 @@
 /*
 myqiu_waf.php 1.0
 www.myqiu.org
+////////////////////
+ver1.1
+updete 2020.1.17 
+修复了日志
+日志中增加了提交内容
+日志中增加了时间
 */
+
 $getfilter="select|(and|or)\\b.+?(>|<|=|in|like)|\\/\\*.+?\\*\\/|<\\s*script\\b|\\bEXEC\\b|UNION.+?SELECT|UPDATE.+?SET|INSERT\\s+INTO.+?VALUES|(SELECT|DELETE).+?FROM|(CREATE|ALTER|DROP|TRUNCATE)\\s+(TABLE|DATABASE)";
 $postfilter="\\b(and|or)\\b.{1,6}?(=|>|<|\\bin\\b|\\blike\\b)|\\/\\*.+?\\*\\/|<\\s*script\\b|\\bEXEC\\b|UNION.+?SELECT|UPDATE.+?SET|INSERT\\s+INTO.+?VALUES|(SELECT|DELETE).+?FROM|(CREATE|ALTER|DROP|TRUNCATE)\\s+(TABLE|DATABASE)";
 $cookiefilter="\\b(and|or)\\b.{1,6}?(=|>|<|\\bin\\b|\\blike\\b)|\\/\\*.+?\\*\\/|<\\s*script\\b|\\bEXEC\\b|UNION.+?SELECT|UPDATE.+?SET|INSERT\\s+INTO.+?VALUES|(SELECT|DELETE).+?FROM|(CREATE|ALTER|DROP|TRUNCATE)\\s+(TABLE|DATABASE)";
 function Codepage($strgets,$strgetsq,$strgetsql){
 	if(preg_match("/".$strgetsql."/is",$strgetsq)==1){
-	$url='http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING'];
+        $url='http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING'];
+        $hackclientip=$_SERVER['REMOTE_ADDR'] ;
+        http_response_code(502);
 	echo '<br>You have triggered the WAF protection rules';
+		echo '<br>502 Bad Gateway Error';
         echo '<br>你已经触发waf防护规则';
-        echo '<br>提交地址危险地址'.$url;
-        $sqlfile='log.txt';
-        $content='$url\r\n';
+        echo '<br>你的IP是'.$hackclientip;
+        echo '<br>提交地址为'.$url;
+        $POSTSTRING=http_build_query($_POST);
+        $GETSTRING=http_build_query($_GET);
+        $COOKIESTRING=http_build_query($_COOKIE);
+        echo  $_SERVER['SCRIPT_NAME'];
+        $sqlfile=$_SERVER['DOCUMENT_ROOT'].'/badlog.txt' ;
+        $content='time:'.date("Y-m-d h:i:s").'  ip:'.$hackclientip.'  url:'.$url.' post:'.$POSTSTRING.'  get:'.$GETSTRING.'  cookie:'.$COOKIESTRING."\r\n";
         if($sqlfile_url=file_put_contents($sqlfile,$content,FILE_APPEND)){
-        echo '你的操作记录已经被记录';
-
+        echo '<br>你的操作记录已经被记录!';
+        
 }
-        //echo '<script>window.close();</script>';
 	exit();	
 }
 }
